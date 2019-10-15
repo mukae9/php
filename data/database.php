9c619ba656664600d425b9c183a51e10ec92c8b1
mukae9 <rockjoint2016@gmail.com>
2019-10-15 23:16:21 +0900

    Insert変更

--- data/database.php
+++ data/database.php
@@ -1,20 +1,41 @@
+<?php
+// エラーを出力する
+ini_set('display_errors', "On");
+?>
+
 <?php echo "データベース接続をしてみます";
- print date('G');
 
 
     try {
-            # hostには「docker-compose.yml」で指定したコンテナ名を記載
             require("mysql.php");
-
             $sql = "SELECT * FROM demo";
             $stmt = $db->prepare($sql);
             $stmt->execute();
             $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
             var_dump($result);
-        } catch (PDOException $e) {
+
+          
+            $name="お名前";
+            $type="";
+            $age="";
+
+            $sql="INSERT INTO demo (name,type,age)VALUES(:name,:type,:age)";
+                $stmt_insert=$db->prepare($sql);
+                $stmt_insert->bindValue(':name', $name , PDO::PARAM_STR);
+                $stmt_insert->bindValue(':type', $type , PDO::PARAM_STR);
+                $stmt_insert->bindValue(':age', $age , PDO::PARAM_STR);
+                    
+                $stmt_insert->execute();
+                var_dump($stmt_insert);
+                if($stmt_insert){
+                    print"成功です";
+                }
+                $db=null;
+
+    } catch (PDOException $e) {
             echo $e->getMessage();
             exit;
-        } 
+    } 
 
         
         
--- data/mysql.php
+++ data/mysql.php
@@ -1,3 +1,3 @@
-<?php
-            $dsn = "mysql:host=db;dbname=sample;";
-            $db = new PDO($dsn, 'user', 'userpass');
+<?php       
+    $dsn = "mysql:host=db;dbname=sample;";
+    $db = new PDO($dsn, 'user', 'userpass');
