9517c376536b8cf26476193a43f3c1e155d81718
mukae9 <rockjoint2016@gmail.com>
2019-10-25 19:39:59 +0900

    Update database.php

--- data/database.php
+++ data/database.php
@@ -10,14 +10,14 @@
 
           
             $name="お名前";
-            $type="";
+            $email="demo@cf.commm";
             $age="";
 
             $sql="INSERT INTO demo (name,type,age)VALUES(:name,:type,:age)";
                 $stmt_insert=$db->prepare($sql);
                 $stmt_insert->bindValue(':name', $name , PDO::PARAM_STR);
-                $stmt_insert->bindValue(':type', $type , PDO::PARAM_STR);
-                $stmt_insert->bindValue(':age', $age , PDO::PARAM_STR);
+                $stmt_insert->bindValue(':email', $email , PDO::PARAM_STR);
+                $stmt_insert->bindValue(':age', $age , PDO::PARAM_INT);
                     
                 $stmt_insert->execute();
                 var_dump($stmt_insert);
@@ -36,10 +36,10 @@
 ?>
 
  <p>データベース接続をしてみます。<br>
- localhost:4040にアクセスすると、データベースを管理できるphpMyadminが開きます.
+ <a href="localhost:4040">localhost:4040</a>にアクセスすると、データベースを管理できるphpMyadminが開きます.
  <br>
  user userpassでログインできます。<br>
- 今はデータを管理するテーブルが一つもないので、教科書を参考にしつつdemoテーブルを作り、
- idカラム,nameカラム,typeカラム,ageカラムの構造にしてください。<br>
+ 今はデータを管理するテーブルが一つもないので、教科書を参考にしつつuserテーブルを作り、
+ idカラム,nameカラム,ageカラム,emailカラムの構造にしてください。(教科書参考)<br>
  この画面を更新してみると上手く行けばどんどん情報が追加されていきます。<br>
  データベースからの情報の取得と挿入ができいる証拠です。</p>
